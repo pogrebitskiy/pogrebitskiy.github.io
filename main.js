3a0c732fb7ffdffd7b13228720cfe7ad7b8eb263
pogrebitskiy <pogrebitskiy.david@gmail.com>
2023-04-15 20:40:16 -0400

    jump to top button

--- main.js
+++ main.js
@@ -1,8 +1,19 @@
+// Smooth scrolling when using navbar
 document.querySelectorAll('a[href^="#"]').forEach(anchor => {
     anchor.addEventListener('click', function (e) {
       e.preventDefault();
       document.querySelector(this.getAttribute('href')).scrollIntoView({
         behavior: 'smooth'
       });
+    });
+  });
+
+// Scroll to top button
+const scrollToTopButton = document.getElementById('scroll-to-top');
+
+  scrollToTopButton.addEventListener('click', () => {
+    window.scrollTo({
+      top: 0,
+      behavior: 'smooth'
     });
   });
